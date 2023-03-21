d6280ca56a5498aad543fae778af95884f3c44a4
Gabtoriel <gabtorielvixgmail.com>
2023-03-21 02:46:37 +0000

    added some documentary to my readme.md file

--- 0x01-python-if_else_loops_functions/3-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabet.py
@@ -3,4 +3,4 @@
 for char in range(97, 123):
     if char == 101 or char == 113:
         continue
-    print("{}".format(chr(char), end="")
+    print("{}".format(chr(char)), end="")
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,1 @@
+# Here lies my python practice files with the if, else, loops and functions constructs of the python language.
