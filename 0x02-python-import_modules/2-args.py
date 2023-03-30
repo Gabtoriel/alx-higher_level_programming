6e6db174d88df3d49ce8770ae5cb3d8f37eedadc
Victor Gabriel <gabtorielvix@gmail.com>
2023-03-30 15:46:38 +0100

    used the from keyword so that a particular definition from the file can be imported

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-
+from sys import argv
 def main():
-    import sys
-    count = len(sys.argv) - 1
+    count = len(argv) - 1
     if count == 0:
         print("0 arguments.")
     elif count == 1:
-        print("1 argument:")
+        print("{} argument:".format(count), end="\n")
+        print("{}: {}".format(count, argv[1]))
     else:
         print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        for i in range(count):
+            print("{}: {}".format(i + 1, argv[i + 1]), end="\n")
 
 if __name__ == "__main__":main()
