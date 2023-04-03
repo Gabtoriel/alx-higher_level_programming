3a564816be65a7772e50d5022117357074335e2b
Gabtoriel <gabtorielvixgmail.com>
2023-04-03 13:26:15 +0000

    created an empty class the python file

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Class to describe a rectangle object"""
+
+
+class Rectangle:
+
+    """Represents a rectangle object"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,2 @@
+# This directory contains my alx python classes practice project
+. 0-rectangle.py: simple rectangle class definition
