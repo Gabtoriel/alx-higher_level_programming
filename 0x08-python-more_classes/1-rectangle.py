3ec2a2dc17c9f72fccda3b42024705ebde7f8c34
Gabtoriel <gabtorielvixgmail.com>
2023-04-05 14:59:50 +0000

    removed excess whitespaces and tabs from making it pycodestyle compliant

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,28 +7,26 @@ class Rectangle:
 
     """Creates a rectangle object."""
 
-
     def __init__(self, width=0, height=0):
-        
+
         """Initailizes every rectangle object."""
-        
-        	
+
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
-             
+
         """Returns(gets) the width of rectangle."""
-        
+
         return self.__width
-    
+
     @width.setter
     def width(self, value):
-        
+
         """Sets the width of the rectangle object."""
 
-        if type(value) != type(0):
+        if isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -36,28 +34,29 @@ class Rectangle:
 
     @property
     def height(self):
-	
+
         """Gets or returns the height of the rectangle."""
 
         return self.__height
 
     @height.setter
     def height(self, value):
-	
+
         """Sets the height of the rectangle."""
 
-        if type(value) != type(0):
+        if isinstance(value, int):
             raise TypeError("height must be integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
 
 if __name__ == "__main__":
     a = Rectangle(10, 20)
     print(a.height)
     print(a.width)
     a.height = 5
-    a.width = 10 
+    a.width = 10
     a.height = 10.5
     a.width = 5.5
     a.height = -10
