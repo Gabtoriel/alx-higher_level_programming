f01a0ae9f279daa4a28008909c14bec5debe626d
Gabtoriel <gabtorielvixgmail.com>
2023-04-06 13:55:01 +0000

    used property types to intialize the class

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,8 +11,8 @@ class Rectangle:
 
         """Initailizes every rectangle object."""
 
-        self.__width = self.width(width)
-        self.__height = self.height(height)
+        self.__width = self.width = width
+        self.__height = self.height = height
 
     @property
     def width(self):
@@ -26,7 +26,7 @@ class Rectangle:
 
         """Sets the width of the rectangle object."""
 
-        if isinstance(value, int):
+        if not (isinstance(value, int)):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -44,7 +44,7 @@ class Rectangle:
 
         """Sets the height of the rectangle."""
 
-        if isinstance(value, int):
+        if not (isinstance(value, int)):
             raise TypeError("height must be integer")
         if value < 0:
             raise ValueError("height must be >= 0")
