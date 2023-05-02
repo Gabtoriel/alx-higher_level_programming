ae7b2a31876bca9822447fac6e83785d56a72c1b
Gabtoriel <gabtorielvixgmail.com>
2023-05-02 15:35:05 +0000

    added the code to return correct values for subclasses

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,9 +5,16 @@
 
 def inherits_from(obj, a_class):
 
-    """Checks if obj inherits from a class (a_class."""
+    """Checks if obj is a subclass of another class."""
 
-    if issubclass(obj, a_class) is True:
+    if issubclass(type(obj), a_class) is True:
         return True
     else:
         return False
+
+
+if __name__ == "__main__":
+    a = True
+    print(inherits_from(a, int))
+    print(inherits_from(a, bool))
+    print(inherits_from(a, object))
