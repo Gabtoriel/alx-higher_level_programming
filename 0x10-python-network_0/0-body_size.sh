5b9c61e4e4c6194f32c6c79f57d5cc71f20f37ad
Gabtoriel <gabtorielvixgmail.com>
2023-07-07 00:08:42 +0000

    script to get return the length of an http response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 # script to get a html page through curl and return the length of the response body
-curl -sI $1 | grep -i "content-length:" | cut -d " " -f 2
+curl -sI "$1" | grep -i "content-length:" | cut -d " " -f 2
