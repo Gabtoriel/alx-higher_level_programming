65fb1929009efaa2130ed732a06c65abf87d5ccd
Gabtoriel <gabtorielvixgmail.com>
2023-07-10 15:16:59 +0000

    updated the script to post data using the correct format

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script to send a post request with data to a url
-curl -s -d "email:test@gmail.com&subject=I will always be here for PLD" -X POST $1
+curl -s -d "email=test@gmail.com&subject=I will always be here for PLD" -X POST $1
