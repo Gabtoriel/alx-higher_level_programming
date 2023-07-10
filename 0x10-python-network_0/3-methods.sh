bb4d3eae13bc93de7482348f6f6cf1c671ea925e
Gabtoriel <gabtorielvixgmail.com>
2023-07-10 15:10:05 +0000

    updated the script to return the allowed http request

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script to retrieve all the http methods allowed by a server
-curl -sIX OPTIONS $1 | grep Allow: | cut -d " " -f 2
+curl -sI -X OPTIONS "$1" | grep -i "Allow:" | cut -d " " -f 2
