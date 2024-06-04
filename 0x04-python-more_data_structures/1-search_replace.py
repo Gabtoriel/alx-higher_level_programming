7b4444c8b58559bdb074d6ada5b8f5eca75d7878
Chibuike Eke <gabtorielvix@gmail.com>
2024-06-04 13:18:57 +0100

    program to search and replace a value in an element.

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+
+
 def search_replace(my_list, search, replace):
-    new_list = list(map(lambda x: replace if x == search else x, my_list))
-    return (new_list)
+    new_list = my_list[:]
+
+    for idx in range(len(new_list)):
+        if new_list[idx] is search:
+            new_list[idx] = replace
+    return new_list
