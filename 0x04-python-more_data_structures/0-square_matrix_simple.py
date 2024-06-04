2bc17cd1d244ff100954a60cc3afa47cf0c1012b
Chibuike Eke <gabtorielvix@gmail.com>
2024-06-04 12:22:47 +0100

    program to return squared values in a two dimensional list.

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+
+
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
-    for i in range(len(matrix)):
-        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
-    return (new_matrix)
+    tmp = [[column ** 2 for column in row]for row in matrix]
+    return tmp
